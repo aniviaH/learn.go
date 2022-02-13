becd6b78bfb5f8828ba3ec5f8ebfe3ce56540f25
1058039938@qq.com <aniviah@163.com>
2022-02-13 21:57:23 +0800

    feat: 第15讲-模块6-锁的种类-同步锁与读写锁

--- chapter06/03.lock/main.go
+++ chapter06/03.lock/main.go
@@ -11,7 +11,9 @@ func main() {
 		//countDict()
 		//countDictGoroutineSafe()
 		//countDictForgetUnlock()
-		countDictLockPrice()
+		//countDictLockPrice()
+
+		countDictGoroutineSafeRW()
 	}
 }
 
@@ -54,6 +56,62 @@ func countDictGoroutineSafe() {
 		}(p)
 	}
 	wg.Wait()
+	fmt.Println("预计有：", 100*counterWorker, "字")
+	fmt.Println("总共有：", totalCount, "字")
+}
+
+func countDictGoroutineSafeRW() {
+	fmt.Println("开始数")
+	const counterWorker int = 5
+
+	var totalCount int64 = 0
+	totalCountLock := sync.RWMutex{}
+
+	var wg = sync.WaitGroup{}
+	wg.Add(counterWorker)
+
+	doneCh := make(chan struct{})
+	for p := 0; p < counterWorker; p++ {
+		go func(p int) { // 读锁可以多个goroutine拿到
+			result := map[int64]struct{}{}
+			//for {
+			fmt.Println(p, "读锁开始时间：", time.Now())
+			totalCountLock.RLock()
+			fmt.Println(p, "读锁拿到锁时间：", time.Now())
+
+			result[totalCount] = struct{}{}
+			totalCountLock.RUnlock()
+
+			time.Sleep(1 * time.Second)
+			//needBreak := false
+			//select {
+			//case <-doneCh:
+			//	needBreak = true
+			//default:
+			//}
+			//
+			//if needBreak {
+			//	break
+			//}
+			//}
+			//fmt.Println("result: ", result)
+		}(p)
+	}
+	for p := 0; p < counterWorker; p++ {
+		go func(p int) {
+			defer wg.Done()
+			//fmt.Println("正在统计第", p, "页, 当前获取totalCount=", totalCount)
+			fmt.Println(p, "写锁开始时间：", time.Now())
+			totalCountLock.Lock()
+			fmt.Println(p, "写锁拿到锁时间：", time.Now())
+			defer totalCountLock.Unlock()
+			totalCount += 100
+			//totalCountLock.Unlock() // 也可以用defer来解锁
+		}(p)
+	}
+	wg.Wait()
+	close(doneCh)
+	time.Sleep(2 * time.Second)
 	fmt.Println("预计有：", 100*counterWorker, "字")
 	fmt.Println("总共有：", totalCount, "字")
 }
