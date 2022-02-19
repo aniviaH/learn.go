f0ebb8325216d95ac3e76577f5a7ade46caa2bfb
1058039938@qq.com <aniviah@163.com>
2022-02-19 23:21:01 +0800

    feat: 关于在goroutine访问goroutine定义外围的循环变量的大坑(类似JS的作用域访问的原理(闭包))

---
+++ chapter06/11.goroutine-trap/goroutine.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	{
+		iarr := []int{1, 2, 3, 4, 5, 6, 7}
+		wg := sync.WaitGroup{}
+		wg.Add(len(iarr))
+		for _, item := range iarr {
+			go func() {
+				defer wg.Done()
+				for i := 0; i < 10; i++ {
+					fmt.Println(item)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+
+	/*{
+		iarr := []int{1, 2, 3, 4, 5, 6, 7}
+		wg := sync.WaitGroup{}
+		wg.Add(len(iarr))
+		for _, item := range iarr {
+			go func(newItem int) {
+				defer wg.Done()
+				for i := 0; i < 10; i++ {
+					fmt.Println(newItem)
+				}
+			}(item)
+		}
+		wg.Wait()
+	}*/
+}
