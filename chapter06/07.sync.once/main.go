8978c18e7c3b140dd3967227ef6442b248b073ee
1058039938@qq.com <aniviah@163.com>
2022-02-19 16:52:59 +0800

    feat: 只做一次的案例--常用的锁类型的版本

---
+++ chapter06/07.sync.once/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type rank struct {
+	standard []string
+}
+
+var globalRank = &rank{}
+var globalRankInitialized bool = false
+var globalRankInitializedLock sync.Mutex
+
+func init() {
+	globalRank.standard = []string{"Asia"}
+}
+
+func initGlobalRankStandard(standard []string, i int) {
+	globalRankInitializedLock.Lock()
+	defer globalRankInitializedLock.Unlock()
+
+	if globalRankInitialized == true {
+		fmt.Println("已初始化过，当前work", i)
+		return
+	}
+	fmt.Println("第", i, "位work, 进行初始化")
+	globalRank.standard = standard
+	globalRankInitialized = true
+}
+
+func main() {
+	standard := []string{"asia"}
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			initGlobalRankStandard(standard, i)
+		}(i)
+	}
+	time.Sleep(3 * time.Second)
+	fmt.Println(globalRank)
+	fmt.Println(globalRankInitialized)
+}
